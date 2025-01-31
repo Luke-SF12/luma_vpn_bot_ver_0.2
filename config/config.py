d49595b5123fbb282956f748a76b653353802511
luk1s <furiya_1200@mail.ru>
2025-01-31 22:00:45 +0300

    ok
    Signed-off-by: luk1s <furiya_1200@mail.ru>

--- config/config.py
+++ config/config.py
@@ -1,5 +1,5 @@
 BOT_TOKEN = "7069430105:AAHDyBT3NEvQQeKCDve38WLZnA5xrnevJf0"
-DB_URL = "postgresql://bot_admin:737999@localhost/luma_vpn_bot"
+DB_URL = "postgresql://luma_user:l121973@db:5432/luma_db"
 
 YOOKASSA_SHOP_ID = "1022594"
 YOOKASSA_SECRET_KEY = "test_zbFKq_YBl6pabRlOMEs5nKsB9TPfkrBojfICZoAuko8"
