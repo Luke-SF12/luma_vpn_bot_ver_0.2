e36df631543d30bf90d7b7deecfd6619845d1818
luk1s <furiya_1200@mail.ru>
2025-01-31 20:18:51 +0300

    ok
    Signed-off-by: luk1s <furiya_1200@mail.ru>

---
+++ config/config.py
@@ -0,0 +1,14 @@
+# config/config.py
+
+BOT_TOKEN = "7069430105:AAHDyBT3NEvQQeKCDve38WLZnA5xrnevJf0"
+DB_URL = "postgresql://bot_admin:737999@localhost/luma_vpn_bot"
+
+YOOKASSA_SHOP_ID = "1022594"
+YOOKASSA_SECRET_KEY = "test_zbFKq_YBl6pabRlOMEs5nKsB9TPfkrBojfICZoAuko8"
+
+# YOOKASSA_SHOP_ID = "1021486"
+# YOOKASSA_SECRET_KEY = "live_o5Hno_jfU2uy6VUY9CW6qLWFvkoOeBNimVLwHDGWUIw"
+
+
+
+ADMINS = [6964184585]
