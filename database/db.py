23f3265339518386ba649f2d52cdf6a3ab901f02
luk1s <furiya_1200@mail.ru>
2025-02-01 15:27:45 +0300

    добавлен payment_link в таблицу payments
    Signed-off-by: luk1s <furiya_1200@mail.ru>

--- database/db.py
+++ database/db.py
@@ -86,7 +86,7 @@ class Database:
                 payment_id TEXT UNIQUE NOT NULL,
                 amount INTEGER NOT NULL,
                 status TEXT DEFAULT 'pending',
-                payment_link TEXT, // jikjklj
+                payment_link TEXT,
                 created_at TIMESTAMP DEFAULT NOW()
 );
 
